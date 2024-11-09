be679b4e91f2c4f83b81f62785583c7914a4c954
ilariondan <100044494+ilariondan@users.noreply.github.com>
2024-11-08 22:50:19 -0600

    Update UnitTests.cs

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using NUnit.Framework;
 
 namespace GithubActionsLab
@@ -10,15 +10,15 @@ namespace GithubActionsLab
         public void Add_Valid_Danilchenko()
         {
             Console.WriteLine("Running test: Add_Valid_Danilchenko");
-            Assert.AreEqual(3, Program.Add("1", "2"));  // This will pass
-            Assert.AreEqual(5, Program.Add("3", "2"));  // This will pass
-            Assert.AreEqual(10, Program.Add("5", "7")); // This will fail intentionally (correct value is 12)
+            Assert.AreEqual(3, Program.Add("1", "2"));
+            Assert.AreEqual(5, Program.Add("3", "2"));
+            Assert.AreEqual(12, Program.Add("5", "7"));
         }
 
         [Test]
         public void Add_Invalid_Danilchenko()
         {    
-            Console.WriteLine("Running test: Add_Invalid_Danilchenko");
+            Console.WriteLine("Running test: Add_Valid_Danilchenko");
             Assert.Throws<FormatException>(() => Program.Add("1", "a"));
             Assert.Throws<FormatException>(() => Program.Add("a", "1"));
             Assert.Throws<FormatException>(() => Program.Add("a", "a"));
@@ -27,7 +27,7 @@ namespace GithubActionsLab
         [Test]
         public void Add_Null_Danilchenko()
         {    
-            Console.WriteLine("Running test: Add_Null_Danilchenko");
+            Console.WriteLine("Running test: Add_Valid_Danilchenko");
             Assert.Throws<ArgumentNullException>(() => Program.Add("1", null));
             Assert.Throws<ArgumentNullException>(() => Program.Add(null, "1"));
             Assert.Throws<ArgumentNullException>(() => Program.Add(null, null));
@@ -36,4 +36,3 @@ namespace GithubActionsLab
         // Implement 3 tests per operation, following a similar pattern as above
     }
 }
-
